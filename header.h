1734e43e206140170c78f9a7e0d88db0d103a8e9
Robby <>
2021-05-14 13:27:01 +0000

    Initial commit

---
+++ header.h
@@ -0,0 +1,22 @@
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+
+#include <iostream>
+
+using namespace std;
+
+void bash()
+{
+  system("bash");
+}
+void rbash() {
+  system("rbash");
+}
+void dash() {
+  system("dash");
+}
+void sh() {
+  system("sh");
+}
+
+#endif // HEADER_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "header.h"
+using namespace std;
+int main() {
+  cout << "Hello World!\n";
+  sh();
+  bash();
+  rbash();
+  dash();
+}
